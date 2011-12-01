610881e17aebf90a56e25f1566cde570aca99ffa
Marco Msp <marcomspp@googlemail.com>
2011-12-01 12:47:47 +0100

    Graph-Klasse angelegt

---
+++ src/graph/Graph.java
@@ -0,0 +1,45 @@
+
+package graph;
+
+public class Graph {
+
+	public Graph(String file) {
+		// TODO Aus Datei einlesen
+
+	}
+
+	public Graph(int length) {
+
+	}
+
+	public int getDistance(Node a, Node b) {
+
+	}
+
+	public Node[] getNeighbors(Node a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Node[] getAllNodes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getLength() {
+
+	}
+
+	public String getNodeName(Node a){
+		// TODO Auto-generated method stub
+		return "Gie�en";
+	}
+
+	public void setLength(int length) {
+
+	}
+
+	public void setDistance(Node a, Node b, int dist) {
+	}
+
+}
