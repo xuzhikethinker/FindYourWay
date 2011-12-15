c316f1e53d4c73a7a3ed1253a8c2f0075ed3c47b
Felix Brueck <felix.brueck@mni.thm.de>
2011-12-15 12:58:47 +0100

    added method-descriptions in IGraph

--- src/graph/IGraph.java
+++ src/graph/IGraph.java
@@ -2,17 +2,52 @@ package graph;
 
 public interface IGraph {
 
+	/**
+	 * Liefert Distanz zwischen zwei Knoten.
+	 * @param a Knoten 1
+	 * @param b Knoten 2
+	 * @return Distanz
+	 */
 	int getDistance(Node a, Node b);
 
+	/**
+	 * Liefert alle Nachbarn eines Knotens.
+	 * @param a Knoten
+	 * @return Alle Nachbarn von a
+	 */
 	Node[] getNeighbors(Node a);
 
+	/**
+	 * Liefert alle vorhandene Knoten.
+	 * @return Die Knoten
+	 */
 	Node[] getAllNodes();
 
+	/**
+	 * Liefert Anzahl der Knoten des Graphens.
+	 * @return Anzahl
+	 */
 	int getLength();
 
+	/**
+	 * Liefert "Namen" eines Knotens.
+	 * @param a Knoten
+	 * @return Knotennamen
+	 */
 	String getNodeName(Node a);
 
+	/**
+	 * Setzt Anzahl der Knoten des Graphens.
+	 * @param length Anzahl
+	 * @post Daten eines vorherigen Graphens werden gelöscht.
+	 */
 	void setLength(int length);
 
+	/**
+	 * Setzt Distanz zwischen zwei Knoten.
+	 * @param a Knoten 1
+	 * @param b Knoten 2
+	 * @param dist Distanzwert
+	 */
 	void setDistance(Node a, Node b, int dist);
 }
