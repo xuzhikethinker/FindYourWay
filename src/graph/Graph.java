b2f83681c59b7b556be7d2b3fe7fa053fb28ed70
Marco <marcomspp@googlemail.com>
2012-01-05 11:38:15 +0100

    Angepasste Beispieldaten

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -12,10 +12,11 @@ public class Graph implements IGraph {
 		// TODO Aus Datei einlesen
 		
 		// Beispieldatensatz, Hardcoded
+		// http://upload.wikimedia.org/wikipedia/commons/4/45/Dijksta_Anim.gif
 		this.setLength(7);
 		this.setDistance(1, 2, 7); // 1 zu 2
 		this.setDistance(1, 3, 9); // 1 zu 3
-		this.setDistance(1, 3, 14); // 1 zu 3
+		this.setDistance(1, 6, 14); // 1 zu 6
 		this.setDistance(2, 3, 10); // 2 zu 3
 		this.setDistance(2, 4, 15); // 2 zu 4
 		this.setDistance(3, 4, 11); // 3 zu 4
@@ -23,9 +24,13 @@ public class Graph implements IGraph {
 		this.setDistance(4, 5, 6); // 4 zu 5
 		this.setDistance(5, 6, 9); // 5 zu 6
 		
+		this.setNodeName(0, "Undefinierter Punkt");
 		this.setNodeName(1, "Punkt 1");
 		this.setNodeName(2, "Punkt 2");
 		this.setNodeName(3, "Punkt 3");
+		this.setNodeName(4, "Punkt 4");
+		this.setNodeName(5, "Punkt 5");
+		this.setNodeName(6, "Punkt 6");
 	}
 
 	public Graph(int length) {
