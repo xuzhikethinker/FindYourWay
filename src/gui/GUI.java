17db0ddadf7f7cde3fca4f2d963474109b3df950
Marcel Nürnberger <marcel@nuernbergerclub.de>
2012-01-19 13:04:08 +0100

    Eingabe, Ausgabe hinzugefügt

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -2,12 +2,23 @@ package gui;
 
 import java.io.FileNotFoundException;
 
+import javax.swing.JOptionPane;
+
 import graph.Graph;
 import algorithm.IAlgorithm;
 
 public class GUI implements IUserinterface {
 
+	String array[]={"OK","Delete","Start","Finish","Options"};
+	Object Miraculi=JOptionPane.showOptionDialog(null," Willst du das Programm wirklich starten?","FindYourWay ",JOptionPane.DEFAULT_OPTION,JOptionPane.OK_CANCEL_OPTION,null,array,"OK");
 	private IAlgorithm algorithm;
+	String anzahl=JOptionPane.showInputDialog("Geben sie die Anzahl der Städte an","Städtezahl");
+	String namen=JOptionPane.showInputDialog("Geben sie die Städtenamen ein","Städtenamen");
+	String p=JOptionPane.showInputDialog("Bitte geben sie eine Zahl ein!","Startzahl");
+   int a=Integer.parseInt(p);
+   String c=JOptionPane.showInputDialog("Bitte geben sie eine Zahl ein","Endzahl");
+   int z=Integer.parseInt(c);
+   
 
 	@Override
 	public void setAlgorithm(IAlgorithm algorithm) {
@@ -27,13 +38,13 @@ public class GUI implements IUserinterface {
 		}
 
 		try {
-			this.algorithm.setStartNode(10); // ID von Berlin
+			this.algorithm.setStartNode(a); // ID von Berlin
 		} catch (IllegalArgumentException e1) {
 			e1.printStackTrace();
 		}
 		
 		try {
-			this.algorithm.setEndNode(12); // ID von M�nchen
+			this.algorithm.setEndNode(z); // ID von M�nchen
 		} catch (IllegalArgumentException e1) {
 			e1.printStackTrace();
 		}
@@ -47,6 +58,7 @@ public class GUI implements IUserinterface {
 		int result[] = this.algorithm.getResult();
 
 		System.out.println("Ergebnis: " + result);
+		JOptionPane.showMessageDialog(null,"Ergebnis:" + result,"Ergebnisse",JOptionPane.DEFAULT_OPTION);
 
 		// BEISPIEL ENDE
 
