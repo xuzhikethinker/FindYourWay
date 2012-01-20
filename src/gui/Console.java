ed4ac04474739792e9b47c10f8cf38cfc320425e
Fabian Müller <fmueller83@googlemail.com>
2012-01-20 13:15:23 +0100

    Added graphical visualization with JUNG Graph library to Console.java

--- src/gui/Console.java
+++ src/gui/Console.java
@@ -3,9 +3,23 @@ package gui;
 import graph.Graph;
 import graph.IGraph;
 
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer;
+
+import java.awt.Color;
+import java.awt.Paint;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+import org.apache.commons.collections15.Transformer;
 
 import algorithm.IAlgorithm;
 
@@ -16,6 +30,10 @@ public class Console implements IUserinterface {
 	private boolean AlgRunned = false;
 
 	private Scanner scan = new Scanner(System.in);
+
+	private static ArrayList<String> onRoute = new ArrayList<String>();
+	private static String startNode;
+	private static String endNode;
 
 	@Override
 	public void setAlgorithm(IAlgorithm algorithm) {
@@ -173,12 +191,94 @@ public class Console implements IUserinterface {
 	private void runDialog() {
 		try {
 			this.algorithm.run();
+			createGraph();
 
 			System.out.println("Fertig berechnet.");
 			AlgRunned = true;
 		} catch (IllegalStateException e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	public class MyVertexFillPaintFunction<V> implements Transformer<V, Paint> {
+
+		public Paint transform(V v) {
+			if (v.equals(startNode))
+				return Color.GREEN;
+			if (v.equals(endNode))
+				return Color.RED;
+			if (onRoute.contains(v)) {
+				return Color.YELLOW;
+			} else {
+				return Color.WHITE;
+			}
+		}
+	}
+	
+	private void printGraph(final DirectedSparseGraph<String, Integer> g, IGraph ig) {
+		JFrame jf = new JFrame();
+		FRLayout frLayout = new FRLayout(g);
+		VisualizationViewer vv = new VisualizationViewer(frLayout);
+		vv.setForeground(Color.black);
+		vv.getRenderer().getVertexLabelRenderer()
+				.setPosition(Renderer.VertexLabel.Position.CNTR);
+		vv.getRenderContext().setVertexLabelTransformer(
+				new ToStringLabeller<Number>());
+		vv.getRenderContext().setVertexFillPaintTransformer(
+				new MyVertexFillPaintFunction<String>());
+        Transformer<Integer,String> stringer = new Transformer<Integer,String>(){
+            public String transform(Integer e) {
+                return "Edge:";
+            }
+        };        vv.getRenderContext().setEdgeLabelTransformer(stringer);
+		jf.getContentPane().add(vv);
+//		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.pack();
+		jf.setVisible(true);
+	}
+
+	private void createGraph() {
+		class Edge {
+			int from;
+			int to;
+			public Edge(int f, int t) {
+				from = f;
+				to = t;
+			}
+		}
+
+		List <Edge> edges = new ArrayList<Edge>();
+		DirectedSparseGraph<String, Integer> g = new DirectedSparseGraph<String, Integer>();
+		int[] nodes = this.graph.getAllNodes();
+		g = new DirectedSparseGraph<String, Integer>();
+		int [] route = this.algorithm.getResult();
+		int ii = 0;
+		
+		
+		for(int node : nodes) {
+			g.addVertex("" + node);
+		}
+
+		for(int i : nodes) {
+			for(int j : nodes) {
+				if ( 0 != this.graph.getDistance(i, j) ) {
+					edges.add(new Edge(i,j));
+				}
+			}
+		}
+
+		for( Edge e : edges ) {
+			g.addEdge(ii++, "" + e.from, "" + e.to);
+		}
+		
+		for ( int r : route ) {
+			onRoute.add("" + r);
+		}
+		
+		startNode = onRoute.get(0);
+		endNode = onRoute.get(onRoute.size() - 1);
+
+		printGraph(g, this.graph);
 	}
 
 	private void showResult() {
