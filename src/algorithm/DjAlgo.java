7f93019c9b23c0a5e1e8cd2050cd7255460e11bd
Moritz Muench <moritz.muench@mni.thm.de>
2012-01-25 00:50:33 +0100

    Neuen Algorithmus hinzugefügt. Test erweitert.

---
+++ src/algorithm/DjAlgo.java
@@ -0,0 +1,202 @@
+package algorithm;
+
+import java.util.ArrayList;
+import graph.IGraph;
+
+public class DjAlgo implements IAlgorithm {
+	private int 	startNode;
+	private int 	endNode;
+	private IGraph	graph;
+	private Node[]	allNodes;
+	private int[]	path;
+	
+	@Override
+	public void setGraph(IGraph graph) {
+		this.graph = graph;
+	}
+
+	@Override
+	public void setStartNode(int node) throws IllegalArgumentException {
+		if(this.graph == null || this.graph.getLength() <= node){
+			throw new IllegalArgumentException("Dieser Punkt existiert nicht");
+		}
+		
+		this.startNode = node;
+	}
+
+	@Override
+	public void setEndNode(int node) throws IllegalArgumentException {
+		if(this.graph == null || this.graph.getLength() <= node){
+			throw new IllegalArgumentException("Dieser Punkt existiert nicht");
+		}
+		
+		this.endNode = node;
+	}
+
+	@Override
+	public void run() throws IllegalStateException {
+		if (this.startNode == -1 || this.endNode == -1 || this.graph == null) {
+			throw new IllegalStateException("Es wurden noch keine Daten gesetzt");
+		} else {
+			init();
+			distanz_update();		
+			createShortestPath();
+		}
+	}
+
+	@Override
+	public int[] getResult() {
+		return path;
+	}
+
+	@Override
+	public int getTotalDistance() {
+		return this.allNodes[this.endNode].getDistance();
+	}
+
+	@Override
+	public int getStartNode() {
+		return this.startNode;
+	}
+
+	@Override
+	public int getEndNode() {
+		return this.endNode;
+	}
+	
+	private void init() {		
+		this.allNodes = new Node[this.graph.getAllNodes().length];
+		
+		for(int i = 0; i < this.allNodes.length; i++) {
+			this.allNodes[i] =  new Node(this.graph.getAllNodes()[i]);
+		}
+		
+		for(int i = 0; i < this.allNodes.length; i++) {
+			Node[] tmpneighbors = new Node[this.graph.getNeighbors(i).length];
+			
+			for(int j = 0; j < tmpneighbors.length; j++) {
+				tmpneighbors[j] = this.allNodes[this.graph.getNeighbors(i)[j]];
+			}
+			
+			this.allNodes[i].setNeighbors(tmpneighbors);
+			
+		}
+		
+		this.allNodes[this.startNode].distance = 0;
+		
+	}
+
+	private void distanz_update() {
+		Node aktnode = this.allNodes[this.startNode];
+		
+		do{ // Solange es noch unbesuchte Knoten gibt
+			aktnode.setVisited();
+			
+			// Alle Nachbarn des aktuellen Knotens
+			for(Node node : aktnode.getNeighbors()) {
+				if(node.getDistance() > aktnode.getDistance() + graph.getDistance(aktnode.getId(), node.getId()) || node.getDistance() == -1){
+					node.setDistance(aktnode.getDistance() + graph.getDistance(aktnode.getId(), node.getId()));
+					node.setPredecessor(aktnode);
+				}
+			}
+			
+			aktnode = chooseNextNode();
+			
+		}while(unvisitedNodes());
+		
+	}
+	
+	private Node chooseNextNode() {
+		int max = Integer.MAX_VALUE;
+		Node tmpnode = null;
+		
+		for(Node node : this.allNodes) {
+			if(!node.isVisited() && node.getDistance() < max && node.distance != -1){
+				max = node.getDistance();
+				tmpnode = node;
+			}
+		}
+		
+		return tmpnode;
+	}
+	
+	private boolean unvisitedNodes() {
+		for(Node node : this.allNodes) {
+			if(!node.visited){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void createShortestPath() {
+		ArrayList<Integer> ar = new ArrayList<Integer>();
+		
+		Node aktnode = this.allNodes[this.endNode];
+		ar.add(aktnode.id);
+		
+		do{
+			aktnode = aktnode.getPredecessor();
+			ar.add(aktnode.id);
+		}while(aktnode.getId() != this.startNode);
+		
+		this.path = new int[ar.size()];
+		
+		for(int i = 0; i < this.path.length; i++){
+			this.path[i] = ar.get((ar.size() - 1)  - i);
+		}
+		
+	}
+	
+	private class Node {
+		private boolean visited;
+		private int 	distance;
+		private Node 	predecessor;
+		private int		id;
+		private Node[]	neighbors;
+		
+		private Node(int id){
+			this.id				= id;
+			this.visited 		= false; 	// Noch nicht besucht
+			this.distance 		= -1; 		// Noch keine Distanz gesetzt
+			this.predecessor 	= null; 	// Kein Vorgänger
+		}
+
+		private boolean isVisited() {
+			return visited;
+		}
+
+		private void setVisited() {
+			this.visited = true;
+		}
+
+		private int getDistance() {
+			return distance;
+		}
+
+		private void setDistance(int distance) {
+			this.distance = distance;
+		}
+
+		private int getId() {
+			return id;
+		}
+
+		public Node[] getNeighbors() {
+			return neighbors;
+		}
+
+		public void setNeighbors(Node[] neighbors) {
+			this.neighbors = neighbors;
+		}
+
+		public Node getPredecessor() {
+			return predecessor;
+		}
+
+		public void setPredecessor(Node predecessor) {
+			this.predecessor = predecessor;
+		}
+	}
+	
+}
--- src/algorithm/MsAlgorithmTest.java
+++ src/algorithm/MsAlgorithmTest.java
@@ -12,9 +12,14 @@ public class MsAlgorithmTest {
 
 	@Test
 	public void testRun() {
-
+		int start 	= 3;
+		int end 	= 8;
+		long time 	= 0;
+		
+		
 		MsAlgorithm msAlgo = new MsAlgorithm();
 		DijkstraAlgorithm dkAlgo=new DijkstraAlgorithm();
+		DjAlgo djAlgo = new DjAlgo();
 		
 		IGraph graph;
 		
@@ -22,15 +27,20 @@ public class MsAlgorithmTest {
 			graph=new Graph("Cities.graph");
 			msAlgo.setGraph(graph);
 			dkAlgo.setGraph(graph);
+			djAlgo.setGraph(graph);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			fail();
 			return;
 		}
 		
-		dkAlgo.setStartNode(3);
-		dkAlgo.setEndNode(8);
+		// DIJKSTSTRAALGORITHM
+		dkAlgo.setStartNode(start);
+		dkAlgo.setEndNode(end);
+		
+		time = System.nanoTime();
 		dkAlgo.run();
+		time = System.nanoTime() - time;
 
 		System.out.print("DkAlgo-Route: - ");
 		for (int node : dkAlgo.getResult()) {
@@ -38,20 +48,47 @@ public class MsAlgorithmTest {
 		}
 		System.out.println();
 		System.out.println("DkAlgo-Distanz: " + dkAlgo.getTotalDistance() + " km");
+		System.out.println("Zeit: " + time/1000 + "µs");
 		
+		System.out.println();
 		
-		msAlgo.setStartNode(3);
-		msAlgo.setEndNode(8);
+		// MSALGORITHM
+		msAlgo.setStartNode(start);
+		msAlgo.setEndNode(end);
+		
+		time = System.nanoTime();
 		msAlgo.run();
-
+		time = System.nanoTime() - time;
+		
 		System.out.print("MsAlgo-Route: - ");
 		for (int node : msAlgo.getResult()) {
 			System.out.print(graph.getNodeName(node) + " - ");
 		}
 		System.out.println();
 		System.out.println("MsAlgo-Distanz: " + msAlgo.getTotalDistance() + " km");
+		System.out.println("Zeit: " + time/1000 + "µs");
+		
+		System.out.println();
+		
+		// DJALGO
+		djAlgo.setStartNode(start);
+		djAlgo.setEndNode(end);
+		
+		time = System.nanoTime();
+		djAlgo.run();
+		time = System.nanoTime() - time;
+
+		System.out.print("DjAlgo-Route: - ");
+		for (int node : djAlgo.getResult()) {
+			System.out.print(graph.getNodeName(node) + " - ");
+		}
+		System.out.println();
+		System.out.println("DjAlgo-Distanz: " + djAlgo.getTotalDistance() + " km");
+		System.out.println("Zeit: " + time/1000 + "µs");
 		
 		assertArrayEquals(dkAlgo.getResult(), msAlgo.getResult());
+		assertArrayEquals(djAlgo.getResult(), msAlgo.getResult());
+		assertArrayEquals(djAlgo.getResult(), dkAlgo.getResult());
 	}
 
 }
